853cb2315d6c9feebef0546a1a91028438468aa6
Andre Behrens <mrandre@gmail.com>
2017-06-22 21:50:28 -0400

    Auto-load th ephotos route, clean up.

--- app/components/photo-form/component.js
+++ app/components/photo-form/component.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-const { Component, get } = Ember;
+const { Component, get, set } = Ember;
 
 export default Component.extend({
   photo: null,
@@ -7,22 +7,17 @@ export default Component.extend({
   didInsertElement() {
     let fileInput = document.getElementById('photoUpload');
     let ember = this;
-    fileInput.addEventListener('change', function(evt) {
+    fileInput.addEventListener('change', (evt) => {
       let file = evt.target.files[0];
       let reader = new FileReader();
       reader.onload = ((theFile) => {
         return function(e) {
           // Render thumbnail.
-          var div = document.getElementById('preview');
-          let imageData = e.target.result;
-          let photo = get(ember, 'photo');
-          photo.get('photos').addObject(Ember.Object.create({
-            'name': theFile.name,
-            'content_type': 'image/jpg',
-            'data': imageData // base64-encoded `String`, or a DOM `Blob`, or a `File`
+          get(ember, 'photo.photos').addObject(Ember.Object.create({
+            name: theFile.name,
+            content_type: 'image/jpg',
+            data: e.target.result
           }));
-          div.innerHTML = ['<img class="thumb" width="600" src="', e.target.result,
-                            '" title="', escape(theFile.name), '"/>'].join('');
         };
       })(file);
       reader.readAsDataURL(file);
